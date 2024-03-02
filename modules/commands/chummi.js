1af359d8bb6f0abb191174d1886e25665cc92c44
Priyanshu Rajput <60501425+priyanshu192@users.noreply.github.com>
2024-03-02 07:20:51 -0800

    Update chummi.js

--- modules/commands/chummi.js
+++ modules/commands/chummi.js
@@ -4,7 +4,7 @@ module.exports.config = {
   version: "1.0.1",
   hasPermssion: 0,
   credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-  description: "chummi",
+  description: "kiss reply",
   commandCategory: "Noprefix",
   usages: "noprefix",
   cooldowns: 5,
